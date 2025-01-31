0966479df520e14f1e8b0620452fd21eb06afe5c
Ryan Shah <ryan.k.shah@gmail.com>
2025-01-31 21:22:33 +0000

    Update test_helper.exs

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,10 +1,11 @@
 ExUnit.start()
 
-# Common test utilities
 defmodule Wavelets.TestHelpers do
+  import ExUnit.Assertions
+
   def assert_close(list1, list2, tolerance \\ 1.0e-10) do
     assert length(list1) == length(list2)
-
+    
     Enum.zip(list1, list2)
     |> Enum.each(fn {a, b} ->
       assert_in_delta(a, b, tolerance)
@@ -14,7 +15,7 @@ defmodule Wavelets.TestHelpers do
   def assert_close_2d(matrix1, matrix2, tolerance \\ 1.0e-10) do
     assert length(matrix1) == length(matrix2)
     assert length(hd(matrix1)) == length(hd(matrix2))
-
+    
     Enum.zip(matrix1, matrix2)
     |> Enum.each(fn {row1, row2} ->
       assert_close(row1, row2, tolerance)
