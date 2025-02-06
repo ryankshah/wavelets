baee91a9a7034a22cf33fda37efd6a06f6b70a03
Ryan Shah <ryan.k.shah@gmail.com>
2025-02-06 21:12:40 +0000

    Update cwt.ex

--- lib/wavelets/cwt.ex
+++ lib/wavelets/cwt.ex
@@ -10,85 +10,114 @@ defmodule Wavelets.CWT do
   """
   def transform_1d(signal, wavelet_fn, scales, dt \\ 1.0, precision \\ :double) do
     signal_length = length(signal)
-
-    # Center signal
     signal_mean = Enum.sum(signal) / signal_length
     centered_signal = Enum.map(signal, &(&1 - signal_mean))
 
+    IO.puts("\nInput signal length: #{signal_length}")
+    IO.puts("Signal mean: #{signal_mean}")
+
     scales
     |> Enum.map(fn scale ->
-      # Get integrated wavelet at this scale
-      {int_psi_re, int_psi_im} = integrate_wavelet(wavelet_fn, scale)
+      IO.puts("\nProcessing scale: #{scale}")
 
-      # Do convolution with integrated wavelet
+      # Get integrated wavelet
+      {int_psi_re, int_psi_im} = integrate_wavelet(wavelet_fn, scale)
+      IO.puts("Integrated wavelet: #{inspect({int_psi_re, int_psi_im})}")
+
+      # Convolve
       convolved = convolve_signal(centered_signal, {int_psi_re, int_psi_im})
+
+      IO.puts(
+        "First few convolution values: #{inspect(Enum.take(convolved, 3))}"
+      )
 
       # Take derivative and scale
       coeffs = derivative_and_scale(convolved, scale)
+      IO.puts("First few coefficients: #{inspect(Enum.take(coeffs, 3))}")
+
+      # Check energy at this scale
+      energy = compute_scale_energy(coeffs)
+      IO.puts("Energy at scale #{scale}: #{energy}")
 
       {scale, coeffs}
     end)
   end
 
   defp integrate_wavelet(wavelet_fn, scale) do
-    # Integration points
-    dx = 1.0 / scale
-    points = -20..20
+    # Integration points - use more points and smaller dx
+    dx = 0.1 / scale
+    points = -50..50
 
     # Get wavelet values
     wavelet_points =
       points
       |> Enum.map(fn i ->
         x = i * dx
-        wavelet_fn.(x)
+        {psi_re, psi_im} = wavelet_fn.(x)
+        IO.puts("Wavelet at x=#{x}: #{inspect({psi_re, psi_im})}")
+        {psi_re, psi_im}
       end)
 
-    # Integrate using trapezoid rule
-    {_, int_re, int_im} =
-      Enum.reduce(wavelet_points, {nil, 0.0, 0.0}, fn {psi_re, psi_im},
-                                                      {prev, acc_re, acc_im} ->
-        case prev do
-          nil ->
-            {{psi_re, psi_im}, acc_re, acc_im}
-
-          {prev_re, prev_im} ->
-            new_re = acc_re + (psi_re + prev_re) * dx / 2
-            new_im = acc_im + (psi_im + prev_im) * dx / 2
-            {{psi_re, psi_im}, new_re, new_im}
-        end
+    # Integrate with trapezoidal rule
+    {int_re, int_im} =
+      Enum.reduce(wavelet_points, {0.0, 0.0}, fn {psi_re, psi_im},
+                                                 {acc_re, acc_im} ->
+        {
+          acc_re + psi_re * dx,
+          acc_im + psi_im * dx
+        }
       end)
 
+    IO.puts("Integral: #{inspect({int_re, int_im})}")
     {int_re, int_im}
   end
 
   defp convolve_signal(signal, {int_psi_re, int_psi_im}) do
     signal_length = length(signal)
 
-    # Full convolution like numpy.convolve
-    -signal_length..signal_length
-    |> Enum.map(fn i ->
-      Enum.reduce(0..(signal_length - 1), {0.0, 0.0}, fn j, {acc_re, acc_im} ->
-        signal_val = Enum.at(signal, j, 0.0)
+    # Do proper convolution
+    result =
+      -signal_length..signal_length
+      |> Enum.map(fn i ->
+        {sum_re, sum_im} =
+          Enum.reduce(0..(signal_length - 1), {0.0, 0.0}, fn j,
+                                                             {acc_re, acc_im} ->
+            signal_val =
+              if j >= 0 and j < signal_length, do: Enum.at(signal, j), else: 0.0
 
-        {
-          acc_re + signal_val * int_psi_re,
-          acc_im + signal_val * int_psi_im
-        }
+            shift = i + j
+
+            # Only convolve when in range
+            if shift >= 0 and shift < signal_length do
+              {
+                acc_re + signal_val * int_psi_re,
+                acc_im + signal_val * int_psi_im
+              }
+            else
+              {acc_re, acc_im}
+            end
+          end)
+
+        {sum_re, sum_im}
       end)
-    end)
+
+    IO.puts("Convolution length: #{length(result)}")
+    result
   end
 
   defp derivative_and_scale(convolved, scale) do
-    # Take derivative using diff
-    convolved
-    |> Enum.chunk_every(2, 1, :discard)
-    |> Enum.map(fn [{re1, im1}, {re2, im2}] ->
-      d_re = re2 - re1
-      d_im = im2 - im1
-      # Scale by -sqrt(scale) like PyWavelets
-      norm = -:math.sqrt(scale)
-      {d_re * norm, d_im * norm}
-    end)
+    result =
+      convolved
+      |> Enum.chunk_every(2, 1, :discard)
+      |> Enum.map(fn [{re1, im1}, {re2, im2}] ->
+        d_re = re2 - re1
+        d_im = im2 - im1
+        norm = -:math.sqrt(scale)
+        {d_re * norm, d_im * norm}
+      end)
+
+    IO.puts("Derivative length: #{length(result)}")
+    result
   end
 
   defp compute_signal_energy(signal) do
