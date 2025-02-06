b67e4037ddf3d9e253c8af85e5661b2d68ee4945
Ryan Shah <ryan.k.shah@gmail.com>
2025-02-06 20:56:55 +0000

    Update cwt.ex
    Fixed scale multiplication in energy computation and combined normalisation into single factor

--- lib/wavelets/cwt.ex
+++ lib/wavelets/cwt.ex
@@ -13,11 +13,8 @@ defmodule Wavelets.CWT do
     signal_mean = Enum.sum(signal) / signal_length
     centered_signal = Enum.map(signal, &(&1 - signal_mean))
 
-    IO.puts("\nOriginal signal energy: #{compute_signal_energy(signal)}")
-
     # Get wavelet integral for normalization
     {wavelet_norm, _} = compute_wavelet_norm(wavelet_fn)
-    IO.puts("Wavelet norm: #{wavelet_norm}")
 
     results =
       scales
@@ -32,28 +29,21 @@ defmodule Wavelets.CWT do
             wavelet_norm
           )
 
-        # Debug energy at this scale
-        scale_energy = compute_scale_energy(coeffs) * scale
-        IO.puts("Scale #{scale} energy: #{scale_energy}")
-
         {scale, coeffs}
       end)
 
-    # Print total energy after transform
+    # Debug energies
     total_energy =
       results
-      |> Enum.map(fn {scale, coeffs} ->
-        compute_scale_energy(coeffs) * scale
-      end)
+      # Remove scale multiplication
+      |> Enum.map(fn {scale, coeffs} -> compute_scale_energy(coeffs) end)
       |> Enum.sum()
-
-    IO.puts("Total transform energy: #{total_energy}")
 
     results
   end
 
   defp compute_wavelet_norm(wavelet_fn) do
-    # Integrate wavelet numerically like PyWavelets
+    # Integrate wavelet numerically
     dx = 0.001
 
     points =
@@ -76,15 +66,7 @@ defmodule Wavelets.CWT do
          signal_length,
          wavelet_norm
        ) do
-    # PyWavelets' window size
     window_size = min(signal_length - 1, round(8 * scale))
-
-    # Get central frequency (ω0=5 for our Morlet)
-    central_freq = 5.0
-    freq = central_freq / scale
-
-    # Debug
-    IO.puts("Scale #{scale} -> freq #{freq}")
 
     0..(signal_length - 1)
     |> Enum.map(fn pos ->
@@ -92,7 +74,6 @@ defmodule Wavelets.CWT do
       start_idx = max(0, pos - half_window)
       end_idx = min(signal_length - 1, pos + half_window)
 
-      # Compute convolution
       {conv_re, conv_im} =
         start_idx..end_idx
         |> Enum.reduce({0.0, 0.0}, fn i, {re_acc, im_acc} ->
@@ -106,8 +87,9 @@ defmodule Wavelets.CWT do
           }
         end)
 
-      # PyWavelets normalization
-      norm = :math.sqrt(dt / scale) / :math.sqrt(wavelet_norm)
+      # Scale normalization
+      # Combined normalization
+      norm = 1.0 / :math.sqrt(scale * wavelet_norm)
       {conv_re * norm, conv_im * norm}
     end)
   end
